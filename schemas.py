93c96d374d86a2f83cea5a113e381eff79dd8b42
Emirhan <emirhan.uzen@gmail.com>
2026-07-15 15:33:18 +0300

    Modeller,Şemalar oluşturuldu endpointler yazıldı.Şimdilik sql lite kullanıyor

---
+++ schemas.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel
+
+class TeacherCreate(BaseModel):
+    name:str
+    title:str
+    department:str
+
+class TeacherResponse(BaseModel):
+    id:int
+    name:str | None =None
+    department:str | None=None
+    title:str
+    class Config:
+        from_attributes=True
+
+
+class StudentResponse(BaseModel):
+    id:int
+    name:str
+    class Config:
+        from_attributes=True
+
+class CourseCreate(BaseModel):
+    name:str
+    teacher_id:int
+
+class CourseResponse(BaseModel):
+    id:int
+    name:str
+    teacher_id:int
+    class Config:
+        from_attributes=True
+
+class TeacherWithCoursesResponse(TeacherResponse):
+    courses: list[CourseResponse]=[]
+    class Config:
+        from_attributes=True
+class StudentCreate(BaseModel):
+    name:str         
+
+class StudentCourseCreate(BaseModel):
+    student_id:int
+    course_id:int
+
+class StudentCourseResponse(BaseModel):
+    id:int
+    student_id:int
+    course_id:int
+    class Config:
+        from_attributes=True   
